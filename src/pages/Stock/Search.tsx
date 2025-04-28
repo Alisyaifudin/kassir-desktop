a4158b5544016e054fc02c55df0f6f1c09c10b2d
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2025-04-28 23:39:13 +0800

    add debounce on search

--- src/pages/Stock/Search.tsx
+++ src/pages/Stock/Search.tsx
@@ -1,16 +1,30 @@
 import { SearchIcon } from "lucide-react";
 import { Input } from "../../components/ui/input";
+import { useDebouncedCallback } from "use-debounce";
+import { useState } from "react";
 
 export function Search({ query, setQuery }: { query: string; setQuery: (v: string) => void }) {
+	const [value, setValue] = useState(query);
+	const debounced = useDebouncedCallback(
+		(value: string) => {
+			setQuery(value);
+		},
+		// delay in ms
+		200
+	);
 	return (
 		<label className="flex gap-2 items-center flex-1">
 			<SearchIcon size={35} />
 			<Input
-				type="text"
-        placeholder="Cari..."
+				type="search"
+				placeholder="Cari..."
 				className="w-full"
-				value={query}
-				onChange={(e) => setQuery(e.currentTarget.value)}
+				value={value}
+				onChange={(e) => {
+					const val = e.currentTarget.value;
+					setValue(val);
+					debounced(val);
+				}}
 			/>
 		</label>
 	);
