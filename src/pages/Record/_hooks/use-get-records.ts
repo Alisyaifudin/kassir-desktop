000a2498241b9628f14e51f99b50b7155ef26ce4
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2025-07-17 12:08:18 +0800

    fix delete record refetch after deleting

--- src/pages/Record/_hooks/use-get-records.ts
+++ src/pages/Record/_hooks/use-get-records.ts
@@ -16,13 +16,13 @@ export function useGetRecords(timestamp: number, { db }: Context) {
 	const date = Temporal.Instant.fromEpochMilliseconds(timestamp).toZonedDateTimeISO(tz);
 	const start = date.startOfDay().epochMilliseconds;
 	const end = date.startOfDay().add(Temporal.Duration.from({ days: 1 })).epochMilliseconds;
-	const promises = Promise.all([
-		db.record.get.byRange(start, end),
-		db.recordItem.get.byRange(start, end),
-		db.additional.get.byRange(start, end),
-		db.discount.get.byRange(start, end),
-	]);
 	const fetch = useCallback(async (): Promise<Result<"Aplikasi bermasalah", Data>> => {
+		const promises = Promise.all([
+			db.record.get.byRange(start, end),
+			db.recordItem.get.byRange(start, end),
+			db.additional.get.byRange(start, end),
+			db.discount.get.byRange(start, end),
+		]);
 		const res = await promises;
 		for (const [errMsg] of res) {
 			if (errMsg) return err(errMsg);
