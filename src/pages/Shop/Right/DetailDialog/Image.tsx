55396fc2fb75c42fdf65d2a0f8d6fef46455fd54
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2025-12-22 23:47:41 +0800

    add proper popover in shop item

--- src/pages/Shop/Right/DetailDialog/Image.tsx
+++ src/pages/Shop/Right/DetailDialog/Image.tsx
@@ -4,67 +4,69 @@ import { Button } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 
 export function Image({ srcs }: { srcs: string[] }) {
-	const [selected, setSelected] = useState(srcs[0]);
-	const index = srcs.findIndex((src) => src === selected);
-	const handlePrev = () => {
-		if (index <= 0) {
-			return;
-		}
-		setSelected(srcs[index - 1]);
-	};
-	const handleNext = () => {
-		if (index + 1 >= srcs.length) {
-			return;
-		}
-		setSelected(srcs[index + 1]);
-	};
-	const handleClick = (index: number) => () => {
-		setSelected(srcs[index]);
-	};
-	return (
-		<div className="flex flex-col gap-1 h-[95%] w-full">
-			<div className="flex-1 justify-center items-center flex">
-				<Button
-					className="h-full px-0"
-					variant="secondary"
-					onClick={handlePrev}
-					disabled={index <= 0}
-					type="button"
-				>
-					<ChevronLeft />
-				</Button>
-				<div className="relative h-[600px] flex-1 flex justify-center items-center">
-					<img className="object-contain h-full" src={selected} />
-				</div>
-				<Button
-					className="h-full px-0"
-					variant="secondary"
-					disabled={index === srcs.length - 1}
-					onClick={handleNext}
-					type="button"
-				>
-					<ChevronRight />
-				</Button>
-			</div>
-			<div className="flex flex-col w-full">
-				<div className="overflow-x-scroll flex items-center gap-1 h-36 overflow-y-hidden">
-					{srcs.map((src, i) => (
-						<button
-							type="button"
-							key={i}
-							className={"h-32 aspect-square p-0.5"}
-							onClick={handleClick(i)}
-						>
-							<img
-								className={cn("object-contain w-full h-full", {
-									outline: selected === src,
-								})}
-								src={src}
-							/>
-						</button>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  const [selected, setSelected] = useState(srcs[0]);
+  const index = srcs.findIndex((src) => src === selected);
+  const handlePrev = () => {
+    if (index <= 0) {
+      return;
+    }
+    setSelected(srcs[index - 1]);
+  };
+  const handleNext = () => {
+    if (index + 1 >= srcs.length) {
+      return;
+    }
+    setSelected(srcs[index + 1]);
+  };
+  const handleClick = (index: number) => () => {
+    setSelected(srcs[index]);
+  };
+  return (
+    <div className="flex flex-col gap-1 w-full">
+      <div className="flex flex-1">
+        <div className="flex-1 justify-center items-center flex">
+          <Button
+            className="h-full px-0"
+            variant="secondary"
+            onClick={handlePrev}
+            disabled={index <= 0}
+            type="button"
+          >
+            <ChevronLeft />
+          </Button>
+          <div className="relative flex-1 flex justify-center items-center">
+            <img className="object-contain h-full" src={selected} />
+          </div>
+          <Button
+            className="h-full px-0"
+            variant="secondary"
+            disabled={index === srcs.length - 1}
+            onClick={handleNext}
+            type="button"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col w-full h-36 ">
+        <div className="overflow-x-scroll flex items-center gap-1 h-36 overflow-y-hidden">
+          {srcs.map((src, i) => (
+            <button
+              type="button"
+              key={i}
+              className={"h-32 aspect-square p-0.5"}
+              onClick={handleClick(i)}
+            >
+              <img
+                className={cn("object-contain w-full h-full", {
+                  outline: selected === src,
+                })}
+                src={src}
+              />
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Shop/Right/DetailDialog/index.tsx
+++ src/pages/Shop/Right/DetailDialog/index.tsx
@@ -1,65 +1,87 @@
-// import {
-// 	Dialog,
-// 	DialogContent,
-// 	DialogHeader,
-// 	DialogTitle,
-// 	DialogTrigger,
-// } from "~/components/ui/dialog";
-// import { Lock } from "lucide-react";
-// import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
-// import { Image } from "./Image";
+import { Lock } from "lucide-react";
+import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
+import { Image } from "./Image";
+import { db } from "~/database";
+import { toast } from "sonner";
+import { image } from "~/lib/image";
+import { log } from "~/lib/utils";
 
 // TODO: PROPER POPOVER
 export function DetailDialog({
-  productId: _productId,
-  stock: _stock,
+  productId,
+  stock,
   index,
-  name: _name,
+  name,
 }: {
   productId: number;
   stock: number;
   name: string;
   index: number;
 }) {
-  return <p className="text-center">{index + 1}</p>;
-  // <Async state={state}>
-  // 	{(srcs) => {
-  // 		if (srcs.length === 0) {
-  // 			return (
-  // 				<Popover>
-  // 					<PopoverTrigger type="button" className="flex items-center">
-  // 						<p style={style[size].text} className="text-center">
-  // 							{index + 1}
-  // 						</p>
-  // 						<Lock size={style[size].icon} />
-  // 					</PopoverTrigger>
-  // 					<PopoverContent className="flex flex-col text-2xl w-fit">
-  // 						<p style={style[size].text}>Id: {productId}</p>
-  // 						<p style={style[size].text}>Stok: {stock}</p>
-  // 					</PopoverContent>
-  // 				</Popover>
-  // 			);
-  // 		}
-  // 		return (
-  // 			<Dialog>
-  // 				<div className="flex items-center">
-  // 					<p className="text-center">{index + 1}</p>
-  // 					<DialogTrigger type="button">
-  // 						<Lock />
-  // 					</DialogTrigger>
-  // 				</div>
-  // 				<DialogContent className="max-w-7xl">
-  // 					<DialogHeader>
-  // 						<DialogTitle className="text-3xl">{name}</DialogTitle>
-  // 						<Image srcs={srcs} />
-  // 						<div className="flex items-center gap-5 text-3xl">
-  // 							<p>Id: {productId}</p>
-  // 							<p>Stok: {stock}</p>
-  // 						</div>
-  // 					</DialogHeader>
-  // 				</DialogContent>
-  // 			</Dialog>
-  // 		);
-  // 	}}
-  // </Async>
+  const [srcs, setSrcs] = useState<string[]>([]);
+  useEffect(() => {
+    async function download() {
+      const [errMsg, res] = await db.image.get.byProductId(productId);
+      if (errMsg) {
+        toast.error("Gagal mencari gambar");
+        log.error(`Failed to fetch image for ${productId} (${name})`);
+        return;
+      }
+      const imgs = await Promise.all(res.map((r) => image.load(r.name, r.mime)));
+      const urls: string[] = [];
+      for (const [errMsg, img] of imgs) {
+        if (errMsg) {
+          log.error(`Failed to load image for ${productId} (${name})`);
+          continue;
+        }
+        urls.push(img);
+      }
+      setSrcs(urls);
+    }
+    download();
+  }, [productId]);
+  if (srcs.length === 0) {
+    return (
+      <Popover>
+        <PopoverTrigger type="button" className="flex items-center">
+          <p className="text-center">{index + 1}</p>
+          <Lock className="icon" />
+        </PopoverTrigger>
+        <PopoverContent className="flex flex-col text-2xl w-fit">
+          <p>Id: {productId}</p>
+          <p>Stok: {stock}</p>
+        </PopoverContent>
+      </Popover>
+    );
+  }
+  return (
+    <Dialog>
+      <div className="flex items-center">
+        <p className="text-center">{index + 1}</p>
+        <DialogTrigger type="button">
+          <Lock className="icon" />
+        </DialogTrigger>
+      </div>
+      <DialogContent className="max-w-4xl">
+        <DialogHeader>
+          <DialogTitle className="text-normal">{name}</DialogTitle>
+          <div className="max-h-[80vh] overflow-hidden">
+            <Image srcs={srcs} />
+          </div>
+          <div className="flex items-center gap-5 text-3xl">
+            <p>Id: {productId}</p>
+            <p>Stok: {stock}</p>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
