8f2114173d46157077606e64b4045a5aed14acaa
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2025-12-24 15:57:24 +0800

    fix stock/product layout

--- src/pages/Stock/Product/style.css.ts
+++ src/pages/Stock/Product/style.css.ts
@@ -3,7 +3,7 @@ import { style } from "@vanilla-extract/css";
 export const css = {
   nav: {
     big: style({
-      maxHeight: "calc(100vh - 44px)"
+      maxHeight: "calc(100vh - 68px)"
     }),
     small: style({
       maxHeight: "calc(100vh - 44px)"
