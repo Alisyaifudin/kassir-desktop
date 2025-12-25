6e2a931f4d2b7a2271010b03d9ca20793069003f
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2025-12-25 20:13:00 +0800

    [fix] update fuzzy search

--- src/pages/Analytics/Product/use-search.ts
+++ src/pages/Analytics/Product/use-search.ts
@@ -1,34 +1,23 @@
-import { useCallback, useMemo } from "react";
-import Fuse, { IFuseOptions } from "fuse.js";
+import createFuzzySearch from "@nozbe/microfuzz";
+import { useMemo } from "react";
 import { Item } from "~/database/product/get-by-range";
 
 export const useItemSearch = (all: Item[], mode: "buy" | "sell", query: string) => {
   const filtered = all.filter((a) => a.mode === mode);
-  const fuse = useMemo(() => {
-    const options: IFuseOptions<Item> = {
-      keys: ["name"],
-      distance: 10,
-      includeScore: true,
-      includeMatches: true,
-      threshold: 0.2,
-      minMatchCharLength: 1,
-    };
-    return new Fuse<Item>(filtered, options);
+  const fuzzy = useMemo(() => {
+    const fuzzy = createFuzzySearch(all, {
+      key: "name",
+      strategy: "aggressive",
+    });
+    return fuzzy;
   }, [filtered]);
 
-  // Typed search function
-  const search = useCallback(
-    (query: string) => {
-      return fuse.search(query);
-    },
-    [filtered]
-  );
   const searched = useMemo(() => {
     if (query.trim() === "") {
       return filtered;
     }
-    const res = search(query);
+    const res = fuzzy(query);
     return res.map((r) => r.item);
-  }, [query, search, filtered]);
+  }, [query, fuzzy, filtered]);
   return searched;
 };
