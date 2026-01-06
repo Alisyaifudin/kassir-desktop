cab75c7d09f62b6b5d31eeaad405df9fbcfd7302
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2026-01-06 20:03:43 +0800

    [fix]: set note

--- src/transaction/transaction/update-note.ts
+++ src/transaction/transaction/update-note.ts
@@ -4,7 +4,7 @@ import { getTX } from "../db-instance";
 export async function note(tab: number, note: string): Promise<DefaultError | null> {
   const tx = await getTX();
   const [errMsg] = await tryResult({
-    run: () => tx.execute(`UPDATE transactions SET tx_fix = $1 WHERE tab = $2`, [note, tab]),
+    run: () => tx.execute(`UPDATE transactions SET tx_note = $1 WHERE tab = $2`, [note, tab]),
   });
   if (errMsg) return errMsg;
   return null;
